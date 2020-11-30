09e1caa846401a0c7367b49deba437fa018e44b9
Kritik Seth <39276404+kritikseth@users.noreply.github.com>
2020-11-30 17:42:04 +0530

    og revert

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 * Accessible to everybody, and reusable in various contexts
 * Open source, commercially usable - MPL-2.0 license
 
-## Documentation-
+## Documentation- [![Documentation Status](https://readthedocs.org/projects/swachhdata/badge/?version=latest)](https://swachhdata.readthedocs.io/en/latest/?badge=latest)
 
 * https://swachhdata.readthedocs.io/en/latest/
 
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_namespace_packages
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='swachhdata',
     version='1.0.1',
     author='Kritik Seth',
     author_email='sethkritik@gmail.com',
     description='Data cleaning made easy with swachhdata',
-    url='https://github.com/kritikseth/swachhdata',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://swachhdata.readthedocs.io/',
     classifiers=[
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
@@ -15,7 +20,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Operating System :: OS Independent',
-        'Development Status :: 4 - Beta'
+        'Development Status :: 5 - Production/Stable'
     ],
     packages=find_namespace_packages(include=['swachhdata', 'swachhdata.*']),
     install_requires=[
